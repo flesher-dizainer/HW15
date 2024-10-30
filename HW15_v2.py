77adff51c975d22a29e868e8914222d197f4f43a
student_petukhov <dizainer@list.ru>
2024-10-30 14:22:43 +0300

    final code v2

--- HW15_v2.py
+++ HW15_v2.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 import json
 from typing import Any
+import random
 
 
 class JsonFile:
@@ -63,9 +64,69 @@ class CitiesSerializer:
         self.cities_object: list[City] = [City(**city_data) for city_data in cities_data]
 
 
+# здесь будут методы игры
+class CityGame:
+    def __init__(self, json_data: list):
+        self.name_gamer = 'Computer'
+        self.flag_human = False
+        self.cities_objects = CitiesSerializer(json_data)
+        self.city_name_compare = ''
+
+    def start(self):
+        int_rand = random.randint(0, len(self.cities_objects.cities_object) - 1)
+        name_city_start = self.cities_objects.cities_object[int_rand].name
+        self.city_name_compare = name_city_start
+        print(f'Начало игры. Ход {self.name_gamer}')
+        return self.check_game_over(name_city_start)
+
+    def turn(self):
+        '''
+        ход игры
+        :return:
+        '''
+        print(f'Ход {self.name_gamer}. Необходимо выбрать город начинающийся на букву {self.city_name_compare[-1].upper()}')
+        city_name = ''
+
+        if self.flag_human:
+            city_name = input('Введите название города: ')
+        else:
+            for city_obj in self.cities_objects.cities_object:
+                if self.city_name_compare[-1].lower() == city_obj.name[0].lower():
+                    if not city_obj.is_used:
+                        city_name = city_obj.name
+        return self.check_game_over(city_name)
+
+    def check_game_over(self, city_name):
+        '''
+        Проверяем наличие города в списке
+        :param city_name: название города
+        :return: результат проверки bool
+        '''
+        print(f'{self.name_gamer} выбрал город с названием {city_name}')
+        for i in range(len(self.cities_objects.cities_object)):
+            if city_name.lower() == self.cities_objects.cities_object[i].name.lower():
+                if not self.cities_objects.cities_object[i].is_used:
+                    self.cities_objects.cities_object[i].is_used = True
+                    self.flag_human = True if not self.flag_human else False
+                    self.name_gamer = 'Computer' if not self.flag_human else 'Human'
+                    self.city_name_compare = city_name
+                    return True
+        print(f'Игрок {self.name_gamer} проиграл, выбрав город с названием {city_name}')
+        return False
+
+
+# в менеджере будем крутить игру
+class GameManager:
+    def __init__(self, json_data: list):
+        self.city_game = CityGame(json_data)
+
+    def __call__(self):
+        if self.city_game.start():
+            while self.city_game.turn():
+                pass
+
+
 if __name__ == '__main__':
     json_file = JsonFile("cities.json")
-    cities_serializer = CitiesSerializer(json_file.read_data())
-    # n = City('Абакан')
-    # for i in cities_serializer.cities_object:
-    #     print(i == n)
+    game_manager = GameManager(json_file.read_data())
+    game_manager()
